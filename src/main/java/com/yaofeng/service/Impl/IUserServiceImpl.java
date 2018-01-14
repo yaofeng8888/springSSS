b9ea21335197a502800fe1676f8904e4ad147ca2
yaofeng8888 <461563387@qq.com>
2018-01-14 20:07:53 +0800

    日志的使用

--- src/main/java/com/yaofeng/service/Impl/IUserServiceImpl.java
+++ src/main/java/com/yaofeng/service/Impl/IUserServiceImpl.java
@@ -2,6 +2,8 @@ package com.yaofeng.service.Impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,21 @@ import com.yaofeng.service.IUserService;
 @Service
 public class IUserServiceImpl implements IUserService {
 	
-	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private IUserDao userDao;
 	
 	@Override
 	public List<User> userlist() {
 		List<User> userlist = null;
+		
 		try {
 			userlist = userDao.userlist();
-		} catch (Exception e) {
+			
+		} catch (RuntimeException e) {
 			// TODO: handle exception
+			logger.error(e.getMessage(),e);
+			e.printStackTrace();
 		}
 		return userlist;
 	}
